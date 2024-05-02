eea6fff850b4bda286e4b9be49f5b45437c2fb17
nicola1397 <nicola1397@outlook.it>
2024-05-02 15:41:55 +0200

    feat: added restaurant migration

---
+++ database/migrations/2024_05_02_132804_create_restaurants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->varchar('name', 200)->notnull();
+            $table->char('p_iva', 11)->notnull()->unique();
+            $table->varchar('image', 250)->nullable();
+            $table->varchar('address', 150)->notnull();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurant');
+    }
+};
