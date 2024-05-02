3dff414852e786a9ab318e3228967406c2ed7e54
AlbertoCavans <alberto96cavani@gmail.com>
2024-05-02 23:49:12 +0200

    Feat - move DishController.php from Guest to Admin + fix return-Response, namespae & use-row6

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
 
 use App\Models\Dish;
 use Illuminate\Http\Request;
@@ -10,7 +13,7 @@ class DishController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+    
      */
     public function index()
     {
@@ -20,7 +23,7 @@ class DishController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+
      */
     public function create()
     {
@@ -31,7 +34,7 @@ class DishController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+
      */
     public function store(Request $request)
     {
@@ -42,7 +45,7 @@ class DishController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Dish  $dish
-     * @return \Illuminate\Http\Response
+   
      */
     public function show(Dish $dish)
     {
@@ -53,7 +56,7 @@ class DishController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Dish  $dish
-     * @return \Illuminate\Http\Response
+
      */
     public function edit(Dish $dish)
     {
@@ -65,7 +68,7 @@ class DishController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Dish  $dish
-     * @return \Illuminate\Http\Response
+ 
      */
     public function update(Request $request, Dish $dish)
     {
@@ -76,7 +79,7 @@ class DishController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Dish  $dish
-     * @return \Illuminate\Http\Response
+    
      */
     public function destroy(Dish $dish)
     {
