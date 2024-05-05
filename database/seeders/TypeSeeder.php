5d96f0d762adba646108cf4525895bec1eadff4d
nicola1397 <nicola1397@outlook.it>
2024-05-06 00:00:09 +0200

    -- savepoint --

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -19,46 +19,55 @@ class TypeSeeder extends Seeder
         $type = new Type;
         $type->label = 'Italiana';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/italy.svg';
         $type->save();
 
         $type = new Type;
         $type->label = 'Asiatica';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/asian.jpg';
         $type->save();
 
         $type = new Type;
         $type->label = 'Messicana';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/mexican.png';
         $type->save();
 
         $type = new Type;
         $type->label = 'Tedesca';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/germany.webp';
         $type->save();
 
         $type = new Type;
         $type->label = 'Sarda';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/sardinia.png';
         $type->save();
 
         $type = new Type;
         $type->label = 'BBQ';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/bbq.webp';
         $type->save();
 
         $type = new Type;
         $type->label = 'Pizza';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/pizza.jpg';
         $type->save();
 
         $type = new Type;
         $type->label = 'Dolci';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/dessert.avif';
         $type->save();
 
         $type = new Type;
         $type->label = 'Pesce';
         $type->color = $faker->hexColor();
+        $type->image = 'assets/types/fish.jpg';
         $type->save();
 
     }
--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -7,40 +7,78 @@
 @section('content')
 
     <section>
-        <div class="container">
+        <div class="container restaurantLayout">
             <a href="{{ route('admin.restaurants.index') }}" class="my-4 btn btn-primary">
                 <i class="fa-solid fa-circle-left fa-beat"></i>
                 Back to the List</a>
+
             <div class="row">
-                <div class="col-md-6">
+                {{-- COLONNA IMMAGINE --}}
+                <div class="col-sm-12 col-md-6">
 
                     <div class="coverImage">
                         <img src="{{ asset('storage/' . $restaurant->image) }}" alt="{{ $restaurant->name }}">
                     </div>
 
                 </div>
+                {{-- COLONNA DATI --}}
+                <div class="col-sm-12 col-md-6 row">
+                    {{-- NOME --}}
+                    <div class="col-12 ">
+                        <h5>Nome Ristorante</h5>
+                        <h1 class="mb-3" class="detailCap">{{ $restaurant->name }}</h1>
+                    </div>
+                    {{-- PROPRIETARIO --}}
+                    <div class="col-sm-12 col-md-6">
+                        <h5>Proprietario</h5>
+                        <p class="detailCap">{{ $restaurant->user->name }} {{ $restaurant->user->last_name }}</p>
+                    </div>
+                    {{-- TELEFONO --}}
+                    <div class="col-sm-12 col-md-6">
+                        <h5>Telefono</h5>
+                        <p class="detailCap">{{ $restaurant->phone }}</p>
+                    </div>
+                    {{-- PARTITA IVA --}}
+                    <div class="col-sm-12 col-md-6">
+                        <h5>Partita Iva</h5>
+                        <p class="detailCap">{{ $restaurant->p_iva }}</p>
+                    </div>
+                    {{-- INDIRIZZO --}}
+                    <div class="col-sm-12 col-md-6">
+                        <h5>Indirizzo</h5>
+                        <p class="detailCap">{{ $restaurant->address }}</p>
+                    </div>
+                    {{-- TIPO RISTORANTE --}}
+                    <div class="col-12">
+                        <h5 class="mb-4">Tipologia Ristorante</h5>
+                        @foreach ($restaurant->types as $type)
+                            {{-- <p class="detailCap">{{ $type->label }}</p> --}}
+                            <div class="badge">
+                                <div class="typeBadge">
+                                    <div class="badgeImg"><img src="{{ asset($type->image) }}" alt=""
+                                            width="100%">
+                                    </div>
+                                    <span>{{ $type->label }}</span>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
 
-                <div class="col-md-6">
-                    <h5>Nome Ristorante</h5>
-                    <h1 class="mb-4" class="detailCap">{{ $restaurant->name }}</h1>
-                    <h5>Proprietario</h5>
-                    <p class="detailCap">{{ $restaurant->user->name }} {{ $restaurant->user->last_name }}</p>
-                    <h5>Telefono</h5>
-                    <p class="detailCap">{{ $restaurant->phone }}</p>
-                    <h5>Partita Iva</h5>
-                    <p class="detailCap">{{ $restaurant->p_iva }}</p>
-                    <h5>Indirizzo</h5>
-                    <p class="detailCap">{{ $restaurant->address }}</p>
-                    <h5>Tipologia Ristorante</h5>
-                    @foreach ($restaurant->types as $type)
-                        <p class="detailCap">{{ $type->label }}</p>
-                    @endforeach
+                    <!-- BUTTON -->
+                    {{-- <div class="col-8">
+                        <a href="{{ route('admin.dishes.index', $restaurant) }}" class="btn btn--action">
+                            <span>MENU</button>
+                        </a>
+                    </div> --}}
+
+                    {{-- <a href="{{ route('admin.dishes.index', $restaurant) }}" class="my-4 btn btn-primary menubutton">
+                        <span>MENU</span>
+                        <div class="icon"><i class="fa-solid fa-bars"></i></div>
+                    </a> --}}
 
 
-                    <a href="{{ route('admin.dishes.index', $restaurant) }}" class="my-4 btn btn-primary">
-                        <i class="fa-solid fa-bars fa-beat"></i>
-                        Menu</a>
                 </div>
+
             </div>
 
 
