55823ca871d40ee0e30f235409245a9ee2dcf953
GrPaladino <paladino.graziano@gmail.com>
2024-05-06 10:07:17 +0200

    Feat: fix footer gap

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron mb-4" >
-    <div class="container py-5">
+<div class="jumbotron mb-4">
+    <div class="container">
         <div class="row justify-content-between align-items-center">
-        <div class="col-6">
-        <img src="{{ asset('assets/') . "/bg-alt.svg" }}" alt="">
-        <!-- <img src="{{ asset('assets/') . "/boolivery_manager.svg" }}" alt=""> -->
-        </div>
-        <div class="col-5">
-        <h1 class="display-2">Diventa partner Boolivery! 🛵🍝</h1>
-        <p class="fs-2 rob4 my-5">Registra la tua attività ed entra a far parte della famiglia di Boolivery! 
-            Aggiungi i tuoi piatti e raggiungi subito nuovi clienti!
-        </p>
-        <div>
-            <!-- BUTTON -->
-            <a href="{{ route('register') }}" class="btn btn--action">
-                <span>Registra la tua attività!</button>
-            </a>
-        </div>
-        </div>
+            <div class="col-6">
+                <img src="{{ asset('assets/') . "/bg-alt.svg" }}" alt="">
+                <!-- <img src="{{ asset('assets/') . "/boolivery_manager.svg" }}" alt=""> -->
+            </div>
+            <div class="col-5">
+                <h1 class="display-2">Diventa partner Boolivery! 🛵🍝</h1>
+                <p class="fs-2 rob4 my-5">Registra la tua attività ed entra a far parte della famiglia di Boolivery!
+                    Aggiungi i tuoi piatti e raggiungi subito nuovi clienti!
+                </p>
+                <div>
+                    <!-- BUTTON -->
+                    <a href="{{ route('register') }}" class="btn btn--action">
+                        <span>Registra la tua attività!</button>
+                    </a>
+                </div>
+            </div>
 
         </div>
-        
-@endsection
+
+        @endsection
