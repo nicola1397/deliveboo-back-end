c7cc18af12ed3792f76581a7aa54ebcaabbf3973
nicola1397 <nicola1397@outlook.it>
2024-05-07 10:47:04 +0200

    feat: added delete modal to dish show

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -82,6 +82,43 @@
     </section>
 @endsection
 
+{{-- ! Modal --}}
+@section('modal')
+    {{-- * Foreach of the modal --}}
+    <div class="modal fade" id="dish-{{ $dish->id }}" tabindex="-1" aria-labelledby="dish-{{ $dish->id }}"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    {{-- Modal title --}}
+                    <h1 class="modal-title fs-5 text-danger" id="exampleModalLabel">Delete {{ $dish->name }}?</h1>
+                    {{-- Close modal button --}}
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                {{-- Modal main text --}}
+                <div class="modal-body text-black">
+                    You CANNOT revert this action
+                </div>
+                {{-- Modal buttons area --}}
+                <div class="modal-footer">
+                    {{-- Close modal button --}}
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abort</button>
+                    {{-- Delete dish miniform --}}
+                    <form action="{{ route('admin.dishes.destroy', $dish) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        {{-- Delete dish button --}}
+                        <button class="btn btn-danger">Delete</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+
+
 {{-- ! CSS --}}
 @section('css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
