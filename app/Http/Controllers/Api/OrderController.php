a1e0c5b81fd272ac37ce54961cf609755d0d75d9
NicolaTeora <nikxodia@gmail.com>
2024-05-09 13:01:52 +0200

    fix credential token

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Braintree\Gateway;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function generate(Request $request)
+    public function generate(Request $request, Gateway $gateway)
     {
-        return 'generate';
+        $token = $gateway->clientToken()->generate();
+        $data = [
+            'success' => true,
+            'token' => $token
+        ];
+
+        return response()->json($data, 200);
     }
 
-    public function makePayment(Request $request)
+    public function makePayment(Request $request, Gateway $gateway)
     {
         return 'makePayment';
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      *
-     * @return void
+     * 
      */
     public function boot()
     {
