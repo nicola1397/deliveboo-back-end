795a29a78641da13b64d9e8bf2615f712666f391
GrPaladino <paladino.graziano@gmail.com>
2024-05-10 15:35:09 +0200

    Feat:add color on types table

--- database/migrations/2024_05_02_135850_create_types_table.php
+++ database/migrations/2024_05_02_135850_create_types_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -17,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('label', 200);
             $table->string('image', 250)->nullable();
+            $table->string('color', 7);
             $table->timestamps();
         });
     }
